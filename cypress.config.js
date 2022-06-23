4fe716f26e96bed2b7bfa6682174336f70fa80ba
Oleh Babenko <olegbabenko12@ukr.net>
2022-06-23 15:25:31 +0300

    Changed the code

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    
     baseUrl: 'http://automationpractice.com/index.php',
-
-    "env": {
-     "baseUrl":'http://automationpractice.com/index.php'
-  },
 }
 });
