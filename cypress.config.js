4893fc9efa24be0e92396bce0b66a5349fd1a380
Oleh Babenko <olegbabenko12@ukr.net>
2022-06-24 15:31:58 +0300

    Change the configuration file
    Disables video recording and changed browser viewport

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
-const { defineConfig } = require("cypress");
-
+const {defineConfig} = require("cypress");
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://automationpractice.com/index.php',
-}
+    baseUrl: "http://automationpractice.com/index.php",
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    video: false
+  },
 });
