998b1c8763772773914496d24226703a6e35f4bc
Oleh Babenko <olegbabenko12@ukr.net>
2022-06-26 17:55:22 +0300

    Corrected mistakes
    Files  that are updated without errors that have been detected by reviewer

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
-const {defineConfig} = require("cypress");
+const {defineConfig} = require('cypress');
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://automationpractice.com/index.php",
+    baseUrl: 'http://automationpractice.com/index.php',
     viewportWidth: 1920,
     viewportHeight: 1080,
     video: false
