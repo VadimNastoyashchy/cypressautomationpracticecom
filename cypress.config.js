e4d5974447c2f891b1aeee6dadd392414621b067
Oleh Babenko <olegbabenko12@ukr.net>
2022-07-04 17:54:28 +0300

    cypress-mochawesome-reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,15 @@
 const {defineConfig} = require('cypress');
 module.exports = defineConfig({
   projectId: '8jtx9u',
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
-    baseUrl: 'http://automationpractice.com/',
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
     viewportWidth: 1920,
     viewportHeight: 1080,
     video: false,
+    baseUrl: 'http://automationpractice.com/',
   },
 });
+
