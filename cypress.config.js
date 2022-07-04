c119d7fff9aa9e64fefe0cfa7c65d6d559de60c2
Vadym <Vadym_Nastoiashchyi@epam.com>
2022-07-04 18:17:19 +0300

    Fixed reporter
    Fixed reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,19 @@
 const {defineConfig} = require('cypress');
 module.exports = defineConfig({
-  projectId: '8jtx9u',
-  reporter: 'cypress-mochawesome-reporter',
-  e2e: {
-    setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+    projectId: '8jtx9u',
+    reporter: 'mochawesome',
+    reporterOptions: {
+        charts: false,
+        html: true,
+        json: false,
+        reportDir: 'reports',
+        reportFilename: 'report'
     },
-    viewportWidth: 1920,
-    viewportHeight: 1080,
-    video: false,
-    baseUrl: 'http://automationpractice.com/',
-  },
+    e2e: {
+        viewportWidth: 1920,
+        viewportHeight: 1080,
+        video: false,
+        baseUrl: 'http://automationpractice.com/',
+    },
 });
 
