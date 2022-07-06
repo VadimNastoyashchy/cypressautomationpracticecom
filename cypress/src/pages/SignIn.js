d86d47f1b8a72776b2ad9d2a766d66b69597c310
Oleh Babenko <olegbabenko12@ukr.net>
2022-07-06 15:37:48 +0300

    Autologin Sign in page

--- cypress/src/pages/SignIn.js
+++ cypress/src/pages/SignIn.js
@@ -1,7 +1,29 @@
 import BasePage from '../base/BasePage';
 
-export default class SignIn extends BasePage{
-    constructor() {
-        super('index.php?controller=authentication&back=my-account');
-    }
+export default class SignIn extends BasePage {
+  constructor() {
+    super('index.php?controller=authentication&back=my-account');
+  }
+  signInPage_login = 'input[id="email"';
+  signInPage_password = 'input[type="password"';
+  signInPage_loginButtom = '#SubmitLogin';
+
+  enterUsername(username) {
+    cy.get(this.signInPage_login).type(username);
+  }
+  enterPassword(password) {
+    cy.get(this.signInPage_password).type(password);
+  }
+  clickLogin() {
+    cy.get(this.signInPage_loginButtom).click();
+  }
+  /*email(){
+return cy.get('input[id="email"');
+}
+password(){
+    return cy.get('input[type="password"');
+}
+SignInBtn(){
+    return cy.get('#SubmitLogin').contains('Sign in');
+}*/
 }
