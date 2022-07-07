917116c09a52f438122646495504e679c29f73ea
Vadym <Vadym_Nastoiashchyi@epam.com>
2022-07-07 16:46:16 +0300

    Test

--- cypress/e2e/logIn.cy.js
+++ cypress/e2e/logIn.cy.js
@@ -2,7 +2,7 @@ import Home from '../src/pages/Home';
 
 const homePage = new Home();
 
-describe("User Login  tests", () => {
+describe('User Login  tests', () => {
   it('Sign in page (Valid data)', () => {
     homePage
       .visit()
