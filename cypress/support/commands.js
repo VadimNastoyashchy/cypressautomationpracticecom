96f08270a68b8a278aa921e2f34fcd607b4bba70
Oleh Babenko <olegbabenko12@ukr.net>
2022-07-08 14:32:30 +0300

    jsdoc problem

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,27 +1,10 @@
-// Cypress.Commands.add('logInApi', (email, pwd) => {
-//     cy.session([email, pwd], () => {
-//         cy.request({
-//             method: 'POST',
-//             url: 'http://automationpractice.com/index.php?controller=authentication&back=my-account',
-//             body: {
-//                 userEmail: email,
-//                 password: pwd,
-//
-//             }
-//         }).then(res => {
-//             expect(res.status).to.eq(200);
-//             window.localStorage.setItem('token', JSON.stringify(res.body));
-//         });
-//     },
-//         {
-//             validate() {
-//                 cy.visit('http://automationpractice.com/index.php?controller=authentication&back=my-account');
-//
-//
-//             }
-//         }
-//     );
-// });
+/**
+ * user login using the request api.
+ * @constructor
+ * @param {string} email - The user email.
+ * @param {string} password - The user password.
+ */
+
 Cypress.Commands.add('logInApi', (email, password) => {
     cy.request({
         method: 'POST',
