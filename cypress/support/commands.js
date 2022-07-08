aad71ee0bc16dacedb20d0293f385ccdeba00ee9
Vadym <Vadym_Nastoiashchyi@epam.com>
2022-07-08 16:03:57 +0300

    update task

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,10 +1,12 @@
 /**
  * user login using the request api.
- * @constructor
+ * @memberOf cy
+ * @method logInApi
  * @param {string} email - The user email.
  * @param {string} password - The user password.
+ * @returns set cookies
+ * @example cy.logInApi('userName', 'userPassword);
  */
-
 Cypress.Commands.add('logInApi', (email, password) => {
     cy.request({
         method: 'POST',
